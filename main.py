104a4e3c617eaab26918ff82394616903a738e7f
Antos23 <35873201+Antos23@users.noreply.github.com>
2022-05-03 01:26:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,23 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+from game import wordle_game, get_allowed_guesses, select_word
+from random import choice
 
-st.title('Uber pickups in NYC')
+ALLOWED_GUESSES = 6
 
-DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+words = pd.read_csv('unigram_freq.csv')
+common_words = list(words.loc[(words['count']>=1000000)].astype(str).word.values)
+english_5chars_words = [i.upper() for i in common_words if len(i) == 5]
+	
+def main():
 
-@st.cache
-def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
-    lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis='columns', inplace=True)
-    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-    return data
-
-data_load_state = st.text('Loading data...')
-data = load_data(10000)
-data_load_state.text("Done! (using st.cache)")
-
-if st.checkbox('Show raw data'):
-    st.subheader('Raw data')
-    st.write(data)
-
-st.subheader('Number of pickups by hour')
-hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
-st.bar_chart(hist_values)
-
-# Some number in the range 0-23
-hour_to_filter = st.slider('hour', 0, 23, 17)
-filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
-
-st.subheader('Map of all pickups at %s:00' % hour_to_filter)
-st.map(filtered_data)
+	target_word = select_word()
+	st.write(target_word)
+		
+	st.markdown("<h1 style='text-align: center; color: #000077;'>WELCOME TO WORDLE</h1>", unsafe_allow_html=True)
+	
+	wordle_game(target_word)
+			
+if __name__ == "__main__":
+    main()
